de16ac115ee089d0265732c7e3c3f6895fc5206b
Bryan De La Cruz <bryan.delacruza@gmail.com>
2022-01-08 14:53:05 -0500

    cambio link to card detail

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -6,8 +6,7 @@ import { Link } from "react-router-dom";
 const Card = ({ name, image, id }) => {
   return (
     <>
-      <Link to={`/character
-      /${id}`} style={{ textDecoration: "none" }}>
+      <Link to={`/character/${id}`} style={{ textDecoration: "none" }}>
         <div className="card">
           <img src={image} alt="avatar" />
           <h2>{name}</h2>
